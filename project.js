f0525b6272dd372a4277f874ad4aa3e8f872ba2c
KerolosAshraf51 <kerolosibrahim511@gmail.com>
2024-07-01 18:56:09 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+let toggleBtn = document.querySelector("#toggleBtn");
+    
+                let bodyEl = document.querySelector("body");
+    
+                let darkMode = false;
+    
+                toggleBtn.addEventListener('change', (event) => {
+                    darkMode = event.target.checked;
+                    if (darkMode) {
+                        bodyEl.classList.add("dark");
+                    } else {
+                        bodyEl.classList.remove("dark");
+                    }
+                })
+    
